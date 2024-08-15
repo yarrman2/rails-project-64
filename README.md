02e4bd40f5fdc259556a2092635e0c185841149d
yarrrman <yarrrman@yandex.ru>
2024-08-15 15:35:33 +0300

    gitactions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/yarrman2/rails-project-64/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/yarrman2/rails-project-64/actions)
+
+https://rails-project-64-1-q1gn.onrender.com/
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,17 @@
+Sentry.init do |config|
+  config.dsn = 'https://81d68acc2c10fdba92d23133f5990561@o4507781259788288.ingest.de.sentry.io/4507781277941840'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
+end
